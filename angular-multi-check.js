88f8f8f00c653ffcc5d9bac214f875074406b633
emehdy <emehdy@gmail.com>
2016-01-14 23:23:02 +0200

    stable 1

--- angular-multi-check.js
+++ angular-multi-check.js
@@ -6,8 +6,66 @@
 			restrict: 'E',
 			require: 'ngModel',
 			replace:true,
-			template:'<div><h1><input ng-value="model">{{model_ar}} <pre>{{data}}</pre></h1><label ng-repeat="item in src"><input ng-model="data[item]" ng-change="check()" type="checkbox">{{item}}</label></div>',//
-			scope:{model:'=ngModel',src:'='},
+			template:'<div class="multi-check"><label class="multi-check-all" ng-if="allItemsLabel"><input ng-model="all.status" ng-change="all.check()" type="checkbox">{{allItemsLabel}}</label><label ng-repeat="item in items" ng-hide="all.status"><input ng-model="data[item[itemKey]]" ng-change="check()"  type="checkbox">{{item[itemLabel]}}</label></div>',//
+			scope:{
+				model:'=ngModel',
+				items:'=',
+				itemKey:'=',
+				itemLabel:'=',
+				allItemsKey:'=',
+				allItemsLabel:'=',
+			},
+			link: function (scope, element, attrs) 
+			{
+				scope.itemKey=scope.itemKey||'id';
+				scope.itemLabel=scope.itemLabel||'name';
+				scope.allItemsKey=scope.allItemsKey||0;
+				scope.$watch('model',function(val){
+					if(val===0)val='0';val=val||'';
+					scope.model_ar=(val+'').split(',');
+					scope.data={};
+					scope.model_ar.forEach(function(key){scope.data[key]=true;});
+					if(!scope.allItemsLabel)return;
+					if(scope.model!=scope.allItemsKey)return ;
+					scope.data={};
+					scope.all.status=true;
+				});
+				scope.data={};
+				scope.all={status:0,
+				check:function(val){
+					scope.items.forEach(function(item){
+						var key=item[scope.itemKey]||-1;
+						 scope.data[key] =true;
+					})
+					if(scope.all.status)
+						scope.model=scope.allItemsKey;
+					else
+						scope.check();
+					
+				}
+				
+				
+				};
+				scope.check=function( ){
+					scope.model_ar=[];
+					scope.items.forEach(function(item){
+						var key=item[scope.itemKey]||-1;
+						if(scope.data[key])scope.model_ar.push(key);
+					})
+					scope.model=scope.model_ar.join(',');
+				} ;		
+			}
+		}
+    });
+	angular.module('angularMultiCheck')
+	.directive('multiSimpleCheck', function() 
+	{
+		 return {
+			restrict: 'E',
+			require: 'ngModel',
+			replace:true,
+			template:'<div class="multi-check"><label ng-repeat="item in items"><input ng-model="data[item]" ng-change="check()" type="checkbox">{{item}}</label></div>',//
+			scope:{model:'=ngModel',items:'=',itemKey:'='},
 			link: function (scope, element, attrs) 
 			{
 				scope.$watch('model',function(val){
@@ -19,7 +77,7 @@
 				scope.data={};
 				scope.check=function(val){
 					scope.model_ar=[];
-					scope.src.forEach(function(key){
+					scope.items.forEach(function(key){
 						if(scope.data[key])scope.model_ar.push(key);
 					})
 					scope.model=scope.model_ar.join(',');
--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 angular.module('angularMultiCheck', [])
 .controller('demoController',function($scope,$timeout){
 	$scope.colors=['red','green','white','blue'];
-	
+	$scope.colorsRows=[{id:1,name:'red'},{id:2,name:'green'} ]
 	$scope.row={};
+	$scope.name='id';;
 	$timeout(function(){
 		$scope.row.data="red,green,white";
-	},5000)
+		$scope.row.ids1="0";
+		$scope.row.ids="3,4";
+	},1000)
+	
+	$timeout(function(){
+		$scope.colorsRows=[{id:1,name:'red'},{id:2,name:'green'},{id:3,name:'white'},{id:4,name:'blue'}]
+	},4000)
 })
